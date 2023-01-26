445077961ba1f046dcc625558e6fb70c37ef45a1
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-26 11:54:46 +0100

    display quote text

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
               <img src={image} alt={name} className='person-img'/>
               <h4>{name}</h4>
               <p className="title">{title}</p>
+              <p className="text">{quote}</p>
             </article>
           )
         })}
