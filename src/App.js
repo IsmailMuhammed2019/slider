8af12cbcae72684c1261b182d22fc1f971236b8a
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-26 11:42:51 +0100

    create usestate variables.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
 import { FaQuoteRight } from 'react-icons/fa'
 import data from './data'
 function App() {
-  return <h2>slider project setup</h2>
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(0)
+  return (
+    <div className="section">
+      <div className="title">
+        
+      </div>
+    </div>
+
+  )
 }
 
 export default App
+ 
